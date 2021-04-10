e269e19282dba2373de757098b62974b5b162890
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-04-10 15:43:34 -0600

    [10-04-2021::15:.42]- Se agrego saludo desde local

--- AppEjemploGitM51/Program.cs
+++ AppEjemploGitM51/Program.cs
@@ -7,6 +7,7 @@ namespace AppEjemploGitM51
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Saludos desde local");
         }
     }
 }
