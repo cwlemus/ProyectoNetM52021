cb681d7a10f119b35b4c22208255b9086b546a97
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-04-10 15:32:26 -0600

    [10-04-2021:15:32] - Se envio proyectoGt2 por primera vez

---
+++ AppEjemploGit2021M52/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppEjemploGit2021M52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
